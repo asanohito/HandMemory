dbb9365ca39c17d6b8c69799bbac3ee7d85a378f
asanohito <42060469+asanohito@users.noreply.github.com>
2021-07-12 16:50:49 +0900

    画像群１テスト3回目変更

--- js/checkmemory.js
+++ js/checkmemory.js
@@ -46,7 +46,7 @@ function stopCheck() {
   //   }
   // }
 
-  document.getElementById("result").value = caesar(temp, 4);
+  document.getElementById("result").value = caesar(temp, 5);
 
   // var inputElement = document.querySelector('input[name="answer1"]');
   // inputElement.disabled = true;
