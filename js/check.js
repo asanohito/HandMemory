9f91774e79d5db1063671cdc2e00dc296a6136b1
asanohito <42060469+asanohito@users.noreply.github.com>
2021-07-13 17:46:00 +0900

    濱田くん用にcheckimage2作成

--- js/check.js
+++ js/check.js
@@ -47,7 +47,7 @@ function stopCheck() {
   //   }
   // }
 
-  document.getElementById("result").value = caesar(temp, 3);
+  document.getElementById("result").value = caesar(temp, 4);
 
   // var inputElement = document.querySelector('input[name="answer1"]');
   // inputElement.disabled = true;
